b47cf44d09116ef8c5d0ac7c2947fbafcba87267
r0han99 <r409067@gmail.com>
2023-01-11 13:06:27 -0500

    update-v3

--- src/attributions.py
+++ src/attributions.py
@@ -8,7 +8,7 @@ def attribute():
     # for _ in range(20):
     #     st.sidebar.markdown('')
     
-    st.markdown('<center>Attributions</center>',unsafe_allow_html=True)
+    st.markdown('''<center><span style="font-family:syne;font-weight:800; font-size:25px">Attributions</span></center>''',unsafe_allow_html=True)
     st.markdown("> <span style='font-family:Syne;'>Machinery Section</span>",unsafe_allow_html=True)
     content0 = '''The Images are selectively scraped from the Wikipedia using the WikiAPI.'''
     content1 = '''The Car Specific Data, such as the Engine, Dimensions etc, are grabbed from F1Technical.com, through a sophisitcated scraping script that I developed.'''
